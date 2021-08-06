a64d59ebf6aeb36e3429c92304346509e8a26f50
logan <loganjames448@gmail.com>
2021-08-06 13:09:59 -0700

    working away. my emojis are appearing differently than in scrimba. AE not center big or black.

--- my emojies/index.js
+++ my emojies/index.js
@@ -1,11 +1,13 @@
-//1. loop thru the array and log each of them out to the console.
+'strict';
 
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const emojiContainer = document.getElementById("emojiContainer") //2. grab the emoji container div and store it in a variable, log it to the console.
+const 
 
-for (let i = 0; i < myEmojis.length; i++) {
-  console.log(myEmojis[i])
+for (let i = 0; i < myEmojis.length; i++) { //1. loop thru the array and log each of them out to the console.
+  //console.log(myEmojis[i])
+  const emoji = document.createElement('span')
+  emoji.textContent = myEmojis[i]
+  emojiContainer.append(emoji)
 }
 
-//2. grab the emoji container div and store it in a variable, log it to the console.
-
-const emojiContainer = document.getElementById("emojiContainer")
