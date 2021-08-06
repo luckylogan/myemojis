043d8ac0b238eb6aab33c5d36b1f9be3cbdba5eb
logan <loganjames448@gmail.com>
2021-08-06 11:22:12 -0700

    starting to build out the scrimba projects seperate on VSCode, this marks we starting to build the 'my emojis'

---
+++ my emojies/index.js
@@ -0,0 +1,11 @@
+//1. loop thru the array and log each of them out to the console.
+
+const myEmojis = ["👨‍💻", "⛷", "🍲"]
+
+for (let i = 0; i < myEmojis.length; i++) {
+  console.log(myEmojis[i])
+}
+
+//2. grab the emoji container div and store it in a variable, log it to the console.
+
+const emojiContainer = document.getElementById("emojiContainer")
