0afff17a0523b873d9cb3c87ebeeae506db04097
logan <loganjames448@gmail.com>
2021-08-10 13:24:10 -0700

    updating

--- my emojies/index.js
+++ my emojies/index.js
@@ -5,20 +5,30 @@ const emojiContainer = document.getElementById("emoji-container") //2. grab the 
 
 
 
-for (let i = 0; i < myEmojis.length; i++) { //1. loop thru the array and log each of them out to the console.
-  //console.log(myEmojis[i]) ------check to see if it works
-  const emoji = document.createElement('span')
-  emoji.textContent = myEmojis[i]
-  emojiContainer.append(emoji)
-}
+function renderEmojis() {
+
+  for (let i = 0; i < myEmojis.length; i++) { //1. loop thru the array and log each of them out to the console.
+    //console.log(myEmojis[i]) ------check to see if it works
+    const emoji = document.createElement('span')
+    emoji.textContent = myEmojis[i]
+    emojiContainer.append(emoji)
+  } //closing loop
+} //closing the function
+
+renderEmojis()
 
 const pushBtn = document.getElementById("push-btn")
 pushBtn.addEventListener("click", function() {
   const emojiInput = document.getElementById("emoji-input")
-  myEmojis.push(emojiInput.value)
-  emojiInput.value = ""
-  console.log(myEmojis)
-})
+  //the if statement parameter below '(emojiInput.value)' based on if user entered anything into the input. 
+  if (emojiInput.value) { 
+    myEmojis.push(emojiInput.value)
+    emojiInput.value = ""
+    emojiContainer.innerHTML = "" //clearing the old array
+    renderEmojis() //this renders new array 
+  } //closing if statement
+  
+}) //closing addEventListener
 
 
 
